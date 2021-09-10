4da69efd483690a73cd8fb4e4e776a7a83d9a17c
CarlaOnate <carlaonategardella@gmail.com>
2021-09-10 13:58:43 -0500

    Regex recognized from line

--- AnalizadorSintaxis_v1/main.cpp
+++ AnalizadorSintaxis_v1/main.cpp
@@ -2,30 +2,40 @@
 #include <regex>
 #include <fstream>
 
+std::string replaceString(std::string className, std::string match){
+    //Todo: Poner bien esta función
+    std::string replace = "<span class=\"number\">";
+    replace.append(match);
+    replace.append("</span>");
+//    std::regex_replace(match, numbers, replaceString);
+    return replace;
+}
 
 
 int main() {
     std::fstream file("file.txt");
-    std::string line;
+    std::string word;
+    char c;
     std::smatch match;
 
     //Regex
-    std::regex numbers("[[:digit:]]*");
+    std::regex numbersReg("\\d+");
     std::regex delimitadores("");
 
     if(file.is_open()){
-        while(std::getline(file, line)){
-            std::cout << line << "\n" ;
-
-
-            if(std::regex_match(line, match, numbers)){
-                std::cout << match[0];
-                std::string replaceString = "<span class=\"number\">";
-                replaceString.append(match[0]);
-                replaceString.append("</span>");
-                std::regex_replace(line, numbers, replaceString);
-            };
+        while(std::getline(file, word)){
+            std::regex_search(word, match, numbersReg, std::regex_constants::match_flag_type::match_any);
+            for(const auto & i : match){
+                std::cout << i << " |  ";
+            }
         }
+
+//        while(file.get(c)){
+//            word += c;
+//            if(std::regex_match(word, match, numbers)){
+//                replaceString("number", match[0]);
+//            }
+//        }
     }
 }
 
