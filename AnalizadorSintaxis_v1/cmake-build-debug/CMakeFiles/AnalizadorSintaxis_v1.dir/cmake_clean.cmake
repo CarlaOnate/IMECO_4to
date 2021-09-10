9a5f1548a33db81f9abcb50717d5ca54e5fd89a7
CarlaOnate <carlaonategardella@gmail.com>
2021-09-10 11:05:10 -0500

    Inicio analizador texto part 1

---
+++ AnalizadorSintaxis_v1/cmake-build-debug/CMakeFiles/AnalizadorSintaxis_v1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AnalizadorSintaxis_v1"
+  "AnalizadorSintaxis_v1.pdb"
+  "CMakeFiles/AnalizadorSintaxis_v1.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AnalizadorSintaxis_v1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
