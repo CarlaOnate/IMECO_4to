ee86e50d913754243b9ce368676dec90c0cbfe7a
CarlaOnate <carlaonategardella@gmail.com>
2021-09-10 13:28:34 -0500

    Before changing to get line to get char

--- AnalizadorSintaxis_v1/main.cpp
+++ AnalizadorSintaxis_v1/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 #include <regex>
+#include <fstream>
+
+
 
 int main() {
+    std::fstream file("file.txt");
+    std::string line;
+    std::smatch match;
+
+    //Regex
+    std::regex numbers("[[:digit:]]*");
+    std::regex delimitadores("");
+
+    if(file.is_open()){
+        while(std::getline(file, line)){
+            std::cout << line << "\n" ;
 
 
-
-    return 0;
+            if(std::regex_match(line, match, numbers)){
+                std::cout << match[0];
+                std::string replaceString = "<span class=\"number\">";
+                replaceString.append(match[0]);
+                replaceString.append("</span>");
+                std::regex_replace(line, numbers, replaceString);
+            };
+        }
+    }
 }
+
+
