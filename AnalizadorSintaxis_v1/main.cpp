938555c35d05422d9249dcdf6312dcfc3428de54
CarlaOnate <carlaonategardella@gmail.com>
2021-09-11 13:36:29 -0500

    Bugs in identifier or reserved word but does not repeat spans

--- AnalizadorSintaxis_v1/main.cpp
+++ AnalizadorSintaxis_v1/main.cpp
@@ -7,56 +7,76 @@
 //https://regex101.com/
 
 
-
-std::smatch matching(std::string line, std::regex regularEx){
-    std::smatch match;
-    std::regex_search(line, match, regularEx, std::regex_constants::match_flag_type::match_any);
-    return match;
-}
-
-
 std::string replaceMatchforHTML(std::string& line, std::regex reg, int type){
     std::vector<std::string> reservedWords = {"define", "lambda", "if", "cond", "else", "true", "false", "nil", "car", "cdr", "cons", "list", "apply", "map", "let", "begin", "null?", "eq?", "set!"};
 
-//    if(type == 4){
-//        auto it = std::find(reservedWords.begin(), reservedWords.end(), el);
-//        if(it != reservedWords.end()) type = 8;
-//    }
 
-    std::string replaceString = "<span class=";
-        switch (type) {
-            case 0:
-                replaceString.append("\"number\"");
-            break;
-            case 1:
-                replaceString.append("\"decimal\"");
-            break;
-            case 2:
-                replaceString.append("\"exp\"");
-            break;
-            case 3:
-                replaceString.append("\"comment\"");
-            break;
-            case 4:
-                replaceString.append("\"identifier\"");
-            break;
-            case 5:
-                replaceString.append("\"parenthesis\"");
-            break;
-            case 6:
-                replaceString.append("\"quotes\"");
-            break;
-            case 8:
-                replaceString.append("\"reserved\"");
-            break;
-            default:
-                replaceString.append("\"default\"");
-            break;
+    if(type == 4){
+        //Store matches inside vector
+        std::vector<std::string> nonDuplicatesMatches;
+        std::regex_iterator<std::string::iterator> it (line.begin(), line.end(), reg);
+        std::regex_iterator<std::string::iterator> end;
+        while(it != end){
+            nonDuplicatesMatches.push_back(it->str());
+            ++it;
         }
-        replaceString.append("/>");
+        //Removing duplicate matches from vector
+        std::sort(nonDuplicatesMatches.begin(), nonDuplicatesMatches.end());
+        auto last = std::unique(nonDuplicatesMatches.begin(), nonDuplicatesMatches.end());
+        nonDuplicatesMatches.erase(last, nonDuplicatesMatches.end());
+
+        //Replace matches with HTML tag
+        std::string replace;
+        std::string replacedString = line;
+        for(const auto& el : nonDuplicatesMatches){
+            std::cout << el << std::endl;
+            //If match is reseved word then class type is different
+            if(std::find(reservedWords.begin(), reservedWords.end(), it->str()) != reservedWords.end()){
+                replace = "<span class=\"reserved\">";
+            } else {
+                replace = "<span class=\"identifier\">";
+            }
+            replace.append(R"($&)");
+            replace.append("</span>");
+            //Create string for regex -> \b(\w*el\w*)\b
+            std::string regexString = "\\b("; regexString.append(el); regexString.append(")\\b");
+            std::regex reservedReg(regexString);  //Regular expression equal to the match word
+            replacedString = std::regex_replace(replacedString, reservedReg, replace);
+        }
+        return replacedString;
+
+
+    } else {
+        std::string replaceString = "<span class=";
+            switch (type) {
+                case 0:
+                    replaceString.append("\"number\"");
+                break;
+                case 1:
+                    replaceString.append("\"decimal\"");
+                break;
+                case 2:
+                    replaceString.append("\"exp\"");
+                break;
+                case 3:
+                    replaceString.append("\"comment\"");
+                break;
+                case 5:
+                    replaceString.append("\"parenthesis\"");
+                break;
+                case 6:
+                    replaceString.append("\"quotes\"");
+                break;
+                default:
+                    replaceString.append("\"default\"");
+                break;
+            }
+        replaceString.append(">");
         replaceString.append(R"($&)");
         replaceString.append("</span>");
         return std::regex_replace(line, reg, replaceString);
+    }
+
 
 }
 
@@ -66,11 +86,11 @@ int main() {
     std::regex numbersReg(R"(\d+)"); // Fixme: No sirvo - reconozco numeros que son parte de exp
     std::regex decimalNumbersReg(R"(^[+-]?(\d*\.)?\d+$)");  //Fixme: no sirvoo ahaa
     std::regex expNumbersReg(R"(\d+\.\d+[e|E]\d)");  // Todo: test
-    std::regex commentReg(R"(;[[:print:]]*)");  // Todo: Test
+    std::regex commentReg(R"(;.*)");  // Todo: Test
     std::regex identifierReg(R"([a-zA-Z]+[\w*|\-*]*(?!\.\d))");  //Todo: Test
-    std::regex parenthesisReg(R"([\(|\)\{|\}|\[|\]]+)");  //Todo: Test
+    std::regex parenthesisReg(R"([\(|\)\{|\}|\[|\]]+)");
 //    std::regex specialsReg(R"([\+|\-|\*|\/|\<|[<=]|\>|[>=]|\=|\<|\>|\(|\)]*)");  //Fixme: I cause -> The parser did not consume the entire regular expression.
-    std::regex quotesReg(R"([\"|\']\w.*[\"|\']\s)");  //Todo:test
+    std::regex quotesReg(R"([\"|\']\w.*[\"|\']\s)");
 
 
     std::fstream file("file.txt");
@@ -92,13 +112,10 @@ int main() {
 
             for(int i=0; i < regularExps.size(); i++){
 //                // 0 - Numbers, 1 - decimalNum, 2 - expNumber, 3 - comment, 4 - identifier, 5 - parenthesis, 6 - quotes.
-//                // Todo: No guarda varios matches, solo uno - ciclar hasta que no encuentre más match del mismo tipo
                 if(i > 2){
-                    changedLine = replaceMatchforHTML(line, regularExps[i], i);
-                    std::cout << changedLine << "\n";
+                    changedLine = replaceMatchforHTML(changedLine, regularExps[i], i);
                 }
             }
-
             resultFile.push_back(changedLine);
         }
      file.close();
@@ -107,12 +124,13 @@ int main() {
 
     outputFile.open("output.html", std::ios::out | std::ios::app);
     //Todo: Falta poner los header y DOCTYPE!
-
+    outputFile << "<!DOCTYPE html>\n" << "<html>\n" << "<body>\n" << std::endl;
     std::cout << "VECTOR DE RESULTADO \n";
     for(const std::string& el : resultFile){
         std::cout << el << "\n";
         outputFile << el << std::endl;
     }
+    outputFile << "</body>\n" << "</html>\n" << std::endl;
 
     outputFile.close();
 
