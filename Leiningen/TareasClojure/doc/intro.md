44c3e55d8e826fa2febb988bdace502a8740f3cb
CarlaOnate <carlaonategardella@gmail.com>
2021-11-12 15:21:38 -0600

    Added clojure folder

---
+++ Leiningen/TareasClojure/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to probando
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ Leiningen/TareasClojure/project.clj
@@ -0,0 +1,10 @@
+(defproject probando "0.1.0-SNAPSHOT"
+  :description "Aqui aprendiendo otra vez como editar archivos desde la terminal"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :main ^:skip-aot probando.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
