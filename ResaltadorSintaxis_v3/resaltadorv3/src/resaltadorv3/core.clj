d555f0bc69eeec7d6dc14b74edc8b247c5cbe49a
CarlaOnate <carlaonategardella@gmail.com>
2021-11-24 11:38:35 -0600

    Resigned automata and almost all is highlihgted correctly, missing ..

--- ResaltadorSintaxis_v3/resaltadorv3/src/resaltadorv3/core.clj
+++ ResaltadorSintaxis_v3/resaltadorv3/src/resaltadorv3/core.clj
@@ -1,12 +1,11 @@
 (ns resaltadorv3.core
   (:gen-class))
 
-(def estados* '(101 102 105 209))
+(def estados* '(101 102 106))
 (def reservedWords '("println" "def" "if" "cond" "defn" "fn" "else" "true" "false" "nil" "cons" "first" "second" "rest" "next" "concat" "map" "apply" "filter" "reduce" "pmap" "let" "seq" "conj" "do" "and" "or"
 "not" "not=" "quote" "empty?" "take" "range" "doall" "time" "future" "delay" "promise"))
 
-(def listFile (seq (slurp "src/resaltadorv3/datos.txt")))
-;(def filesList )
+(def resHaskell '("case" "class" "data" "default" "deriving" "do" "else" "if" "import" "in" "infix" "infixl" "infixr" "instance" "let" "module" "newtype" "of" "then" "type" "where" "_"))
 
 ; lista transiciones
 (def transiciones '((0 \space 0) (0 \newline 201) (0 "number" 1) (1 "number" 1) (1 "letter-e" 101) (1 \e 101) (1 "special" 101) (1 "parenthesis" 101)
@@ -19,8 +18,25 @@
 (6 \space 6) (6 \. 6) (6 \newline 106)
 (0 \< 7) (0 \> 7) (7 \= 208) (7 "letter-e" 200) (7 \e 200) (7 "number" 200) (7 "special" 200) (7 "parenthesis" 200) (7 \space 200)
 (7 \newline 200)
-(0 "special" 200)
-(0 "parenthesis" 200) (0 \. 200)))
+(0 "special" 200)(0 "parenthesis" 200) (0 \. 200) (0 \newline 0)))
+
+(def haskell '(
+   (0 \space 0) (0 "number" 1) (1 "number" 1) (1 "letter-e" 101) (1 \e 101) (1 \E 101) (1 "special" 101) (1 \space 101) (1 \newline 101) (1 "parenthesis" 101) (1 \{ 101) (1 \- 101) (1 \} 101)
+   (1 \. 2) (2 "number" 2) (2 "letter-e" 102) (2 "special" 102) (2 \space 102) (2 "parenthesis" 102) (2 \newline 102)
+   (2 \e 3) (2 \E 3) (3 \- 3) (3 "number" 4) (4 "number" 4) (4 \space 103) (4 \newline 103) (4 "letter-e" 103)
+   (4 \e 103) (4 \E 103) (4 "special" 103) (4 \{ 103) (4 \} 103) (4 "parenthesis" 103)
+   (0 \" 5) (5 "number" 5) (5 "letter-e" 5) (5 \e 5) (5 \E 5) (5 "special" 5) (5 \space 5) (5 \. 5)
+   (5 \_ 5) (5 \' 5) (5 \- 5) (5 "parenthesis" 5) (5 \newline 5) (5 \" 104)
+   (0 \' 6) (6 "number" 6) (6 "letter-e" 6) (6 \e 6) (6 \E 6) (6 "special" 6) (6 \space 6) (6 "parenthesis" 6) (6 \' 105)
+   (0 "letter-e" 7) (0 \e 7) (0 \_ 7) (7 "letter-e" 7) (7 \e 7) (7 \E 7) (7 \_ 7) (7 "number" 7)
+   (7 \space 106) (7 "special" 106) (7 \newline 106) (7 "parenthesis" 106) (7 \{ 106) (7 \} 106)
+   (7 \. 106) (7 \- 106) (7 \" 106) (7 \' 106)
+   (0 \- 8) (8 \space 200) (8 "special" 200) (8 \- 9) (9 "letter-e" 9) (9 \e 9) (9 \E 9) (9 \_ 9) (9 \- 9) (9 "number" 9) (9 \space 9) (9 "special" 9) (9 "parenthesis" 9)
+   (9 \" 9) (9 \' 9) (9 \{ 9) (9 \} 9) (9 \. 9) (9 \newline 107)
+   (0 \{ 10) (10 \- 11) (11 "letter-e" 11) (11 \e 11) (11 \_ 11) (11 "number" 11) (11 \space 11) (11 "special" 11) (11 "parenthesis" 11)
+   (11 \' 11) (11 \" 11) (11 \. 11) (11 \newline 11) (11 \- 12) (12 \} 107)
+   (0 \newline 201) (0 "parenthesis" 200) (0 "special" 200)
+  ))
 
 ; transforma el caracter para que lo pueda leer el automata
 (defn letterToSymbol [el] ;0(n)
@@ -30,6 +46,15 @@
     (some #(= el %) '(\{ \} \[ \] \( \))) "parenthesis"
     (some #(= el %) '(\+ \- \* \# \/ \\ \' \? \: \, \& \%)) "special"
     :else el))
+
+; transforma el caracter para que lo pueda leer el automata
+(defn letterToSymbol2 [el] ;0(n)
+(cond
+  (some #(= el %) '(\a \b \c \d \f \g \h \i \j \k \l \m \n \o \p \q \r \s \t \u \v \w \x \y \z \A \B \C \D \F \G \H \I \J \K \L \M \N \O \P \Q \R \S \T \U \V \W \X \Y \Z)) "letter-e"
+  (some #(= el %) '(\1 \2 \3 \4 \5 \6 \7 \8 \9 \0)) "number"
+  (some #(= el %) '(\[ \] \( \))) "parenthesis"
+  (some #(= el %) '(\+ \* \/ \% \= \< \> \& \| \? \` \: \\ \$ \! \, \; \@ \#)) "special"
+  :else el)) ; - . e E space newline { } " ' _
 
 (defn estado*? [state]
        (if (some #(= (first state) %) estados*) true false))
@@ -51,9 +76,11 @@
   (returnNumerical (some #(when (pos? %) %) (map (fn [x] (mapAFD x simbolo estado)) transiciones))))
 
 
-(defn reserved? [word]
-      (some (fn [x] (= (apply str (filter #(not= \space %) word)) x)) reservedWords))
+;(defn reserved? [word]
+;      (some (fn [x] (= (apply str (filter #(not= \space %) word)) x)) reservedWords))
 
+(defn reserved? [word]
+      (some (fn [x] (= (apply str (filter #(not= \space %) word)) x)) resHaskell))
 
 (def htmlHead "<!DOCTYPEhtml><html><head><title>SuperMegaCoolResaltador</title><body style=\"white-space: pre\"><div><p>")
 
@@ -66,36 +93,20 @@
    (= (first (first listEl)) 102) (str "<span style=\"color: green\">" (apply str (second listEl)) "</span>")
    (= (first (first listEl)) 103) (str "<span style=\"color: orange\">" (apply str (second listEl)) "</span>")
    (= (first (first listEl)) 104) (str "<span style=\"color: blue\">" (apply str (second listEl)) "</span>")
-   (= (first (first listEl)) 105) (if (reserved? (second listEl))
+   (= (first (first listEl)) 105) (str "<span style=\"color: blue\">" (apply str (second listEl)) "</span>")
+   (= (first (first listEl)) 106) (if (reserved? (second listEl))
         (str "<span style=\"color:#bd42bd\">" (apply str (second listEl)) "</span>")
         (str "<span style=\"color:#5da28c\">" (apply str (second listEl)) "</span>"))
-   (= (first (first listEl)) 106) (str "<span style=\"color:#1a9fe5\">" (apply str (second listEl)) "</span></p><p>")
+   (= (first (first listEl)) 107) (str "<span style=\"color:#1a9fe5\">" (apply str (second listEl)) "</span></p><p>")
    (= (first (first listEl)) 200) (str "<span style=\"color:#e1931e\">" (if (seq? (second listEl)) (apply str (second listEl)) (str (second listEl))) "</span>")
-(= (first (first listEl)) 209) (str "<span style=\"color:#e817a2\">" (apply str (if (seq? (second listEl))
-  (second listEl)
-  (list (second listEl)))) "</span>")
    (= (first (first listEl)) 201) "</p><p>"
    (and (= (first (first listEl)) -1) (= (second listEl) \space)) " "
    :else (str "<span style=\"color:black\">"(apply str (second listEl))"</span>")))
 
 
-(defn writeHTML [resList] (spit "datos.html"
-  (str htmlHead (apply str (map (fn [el] (htmlTag el)) resList)) htmlEnd)))
-
 (defn pwriteHTML [fileName resList]
   (spit (str (subs fileName 0 (- (count fileName) 4)) ".html")
     (str htmlHead (apply str (map (fn [el] (htmlTag el)) resList)) htmlEnd)))
-
-
-(defn findPattern2HTML [file state word res]
- (loop [f file s state w word r res] (when (not (nil? file))
-   (cond
-     (nil? f) (writeHTML (concat r (list (list (list s) w)) (list (list (list "end")))))
-     (= s -1) (recur (next f) 0 (concat w (list (first f))) r)
-     (> s 100) (if (some (fn [x] (= s x)) estados*)
-       (recur (concat (list (first (reverse w))) f) 0 '() (concat r (list (list (list s) (reverse (next (reverse w)))))))
-       (recur f 0 '() (concat r (list (list (list s) w)))))
-  :else (recur (next f) (followAFD transiciones (letterToSymbol (first f)) s) (concat w (list (first f))) r)))))
 
 
 (defn pfindPattern2HTML [filename file state word res]
@@ -106,21 +117,13 @@
       (> s 100) (if (some (fn [x] (= s x)) estados*)
         (recur fname (concat (list (first (reverse w))) f) 0 '() (concat r (list (list (list s) (reverse (next (reverse w)))))))
         (recur fname f 0 '() (concat r (list (list (list s) w)))))
-   :else (recur fname (next f) (followAFD transiciones (letterToSymbol (first f)) s) (concat w (list (first f))) r)))))
+   :else (recur fname (next f) (followAFD haskell (letterToSymbol2 (first f)) s) (concat w (list (first f))) r)))))
 
-(defn main []
-    (findPattern2HTML listFile 0 '() '((("begin")))))
 
-(defn mainMultFiles []
+(defn mainSecuencial []
   (with-open [rdr (clojure.java.io/reader "src/resaltadorv3/test.txt")]
-    (map (fn [filePath] (pfindPattern2HTML filePath (seq (slurp filePath)) 0 '() '())) (doall (line-seq rdr)))))
-
-;(mainp '("src/resaltadorv3/dato1.txt" "src/resaltadorv3/dato2.txt"))
-(defn mainp []
-  (with-open [rdr (clojure.java.io/reader "src/resaltadorv3/test.txt")]
-  (pmap (fn [filePath] (pfindPattern2HTML filePath (seq (slurp filePath)) 0 '() '())) (line-seq rdr))))
+  (map #(pfindPattern2HTML % (seq (slurp %)) 0 '() '()) (doall (line-seq rdr)))))
 
 (defn mainPartition []
   (with-open [rdr (clojure.java.io/reader "src/resaltadorv3/test.txt")]
   (pmap (fn [files] (map #(pfindPattern2HTML % (seq (slurp %)) 0 '() '()) files)) (partition-all 5 (doall (line-seq rdr))))))
-    ;(map (fn [el] pfindPattern2HTML el (seq (slurp el)) 0 '() '())) files) (partition-all 5 (doall (line-seq rdr))))))
