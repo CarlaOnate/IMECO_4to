1b6ae978ebd7dc85c53d355f8c3a397a3a90860d
CarlaOnate <carlaonategardella@gmail.com>
2021-11-25 19:12:11 -0600

    Automata is not correct but it works, satarting documents

--- ResaltadorSintaxis_v3/resaltadorv3/src/resaltadorv3/core.clj
+++ ResaltadorSintaxis_v3/resaltadorv3/src/resaltadorv3/core.clj
@@ -1,14 +1,15 @@
 (ns resaltadorv3.core
   (:gen-class))
 
-(def estados* '(101 102 106))
+(def estados* '(101 102 103 106))
 (def reservedWords '("println" "def" "if" "cond" "defn" "fn" "else" "true" "false" "nil" "cons" "first" "second" "rest" "next" "concat" "map" "apply" "filter" "reduce" "pmap" "let" "seq" "conj" "do" "and" "or"
 "not" "not=" "quote" "empty?" "take" "range" "doall" "time" "future" "delay" "promise"))
 
 (def resHaskell '("case" "class" "data" "default" "deriving" "do" "else" "if" "import" "in" "infix" "infixl" "infixr" "instance" "let" "module" "newtype" "of" "then" "type" "where" "_"))
 
 ; lista transiciones
-(def transiciones '((0 \space 0) (0 \newline 201) (0 "number" 1) (1 "number" 1) (1 "letter-e" 101) (1 \e 101) (1 "special" 101) (1 "parenthesis" 101)
+(def transiciones '((0 \space 0) (0 \newline 201)
+(0 "number" 1) (1 "number" 1) (1 "letter-e" 101) (1 \e 101) (1 "special" 101) (1 "parenthesis" 101)
 (1 \< 101) (1 \= 101) (1 \> 101) (1 \; 101) (1 \space 101) (1 \newline 101)
 (1 \. 2) (2 "number" 2) (2 "letter-e" 102) (2 "special" 102) (2 "parenthesis" 102) (2 \space 102) (2 \newline 102) (2 \e 3)
 (3 "number" 103)
@@ -20,21 +21,30 @@
 (7 \newline 200)
 (0 "special" 200)(0 "parenthesis" 200) (0 \. 200) (0 \newline 0)))
 
+
 (def haskell '(
-   (0 \space 0) (0 "number" 1) (1 "number" 1) (1 "letter-e" 101) (1 \e 101) (1 \E 101) (1 "special" 101) (1 \space 101) (1 \newline 101) (1 "parenthesis" 101) (1 \{ 101) (1 \- 101) (1 \} 101)
-   (1 \. 2) (2 "number" 2) (2 "letter-e" 102) (2 "special" 102) (2 \space 102) (2 "parenthesis" 102) (2 \newline 102)
-   (2 \e 3) (2 \E 3) (3 \- 3) (3 "number" 4) (4 "number" 4) (4 \space 103) (4 \newline 103) (4 "letter-e" 103)
-   (4 \e 103) (4 \E 103) (4 "special" 103) (4 \{ 103) (4 \} 103) (4 "parenthesis" 103)
-   (0 \" 5) (5 "number" 5) (5 "letter-e" 5) (5 \e 5) (5 \E 5) (5 "special" 5) (5 \space 5) (5 \. 5)
-   (5 \_ 5) (5 \' 5) (5 \- 5) (5 "parenthesis" 5) (5 \newline 5) (5 \" 104)
-   (0 \' 6) (6 "number" 6) (6 "letter-e" 6) (6 \e 6) (6 \E 6) (6 "special" 6) (6 \space 6) (6 "parenthesis" 6) (6 \' 105)
-   (0 "letter-e" 7) (0 \e 7) (0 \_ 7) (7 "letter-e" 7) (7 \e 7) (7 \E 7) (7 \_ 7) (7 "number" 7)
-   (7 \space 106) (7 "special" 106) (7 \newline 106) (7 "parenthesis" 106) (7 \{ 106) (7 \} 106)
-   (7 \. 106) (7 \- 106) (7 \" 106) (7 \' 106)
-   (0 \- 8) (8 \space 200) (8 "special" 200) (8 \- 9) (9 "letter-e" 9) (9 \e 9) (9 \E 9) (9 \_ 9) (9 \- 9) (9 "number" 9) (9 \space 9) (9 "special" 9) (9 "parenthesis" 9)
-   (9 \" 9) (9 \' 9) (9 \{ 9) (9 \} 9) (9 \. 9) (9 \newline 107)
-   (0 \{ 10) (10 \- 11) (11 "letter-e" 11) (11 \e 11) (11 \_ 11) (11 "number" 11) (11 \space 11) (11 "special" 11) (11 "parenthesis" 11)
-   (11 \' 11) (11 \" 11) (11 \. 11) (11 \newline 11) (11 \- 12) (12 \} 107)
+   (0 \space 0) (0 \. 200)
+   (0 "number" 1) (1 "number" 1) (1 "letter-e" 101) (1 "special" 101) (1 \space 101) (1 \newline 101)
+   (1 "parenthesis" 101) (1 \{ 101) (1 \- 101) (1 \} 101)
+
+   (1 \. 2) (2 "number" 3) (3 "number" 3) (3 "letter-e" 102) (3 "special" 102) (3 \space 102) (3 "parenthesis" 102) (3 \newline 102)
+
+   (1 \e 15) (1 \E 15) (15 \- 15) (15 "number" 16) (16 "number" 16) (16 \space 103) (16 \newline 103) (16 "special" 103)
+   (16 "parenthesis" 103) (16 \{ 103) (16 \} 103) (16 \- 103) (16 "letter-e" 103) (16 \_ 103)
+
+   (3 \e 4) (3 \E 4) (4 \- 4) (4 "number" 5) (5 "number" 5) (5 \space 103) (5 \newline 103) (5 "special" 103)
+   (5 "parenthesis" 103) (5 \{ 103) (5 \} 103) (5 \- 103) (5 "letter-e" 103) (5 \_ 103)
+
+   (0 \" 6) (6 "number" 6) (6 "letter-e" 6) (6 \e 6) (6 \E 6) (6 "special" 6) (6 \space 6) (6 \. 6)
+   (6 \_ 6) (6 \' 6) (6 \- 6) (6 "parenthesis" 6) (6 \newline 6) (6 \" 104)
+   (0 \' 7) (7 "number" 8) (7 "letter-e" 8) (7 \e 8) (7 \E 8) (7 "special" 8) (7 \space 8) (7 "parenthesis" 8) (8 \' 105)
+   (0 "letter-e" 9) (0 \e 9) (0 \E 9) (0 \_ 9) (9 "letter-e" 9) (9 \e 9) (9 \E 9) (9 \_ 9) (9 "number" 9) (9 \' 9)
+   (9 \space 106) (9 "special" 106) (9 \newline 106) (9 "parenthesis" 106) (9 \{ 106) (9 \} 106)
+   (9 \. 106) (9 \- 106) (9 \" 106)
+   (0 \- 10) (10 \space 200) (10 "special" 200) (10 \- 11) (11 "letter-e" 11) (11 \e 11) (11 \E 11) (11 \_ 11) (11 \- 11) (11 "number" 11) (11 \space 11) (11 "special" 11) (11 "parenthesis" 11)
+   (11 \" 11) (11 \' 11) (11 \{ 11) (11 \} 11) (11 \. 11) (11 \newline 107)
+   (0 \{ 12) (12 \- 13) (13 "letter-e" 13) (13 \e 13) (13 \_ 13) (13 "number" 13) (13 \space 13) (13 "special" 13) (13 "parenthesis" 13)
+   (13 \' 13) (13 \" 13) (13 \. 13) (13 \newline 13) (13 \- 14) (14 \} 107)
    (0 \newline 201) (0 "parenthesis" 200) (0 "special" 200)
   ))
 
@@ -91,7 +101,7 @@
  (cond
    (= (first (first listEl)) 101) (str "<span style=\"color: red\">" (apply str (second listEl)) "</span>")
    (= (first (first listEl)) 102) (str "<span style=\"color: green\">" (apply str (second listEl)) "</span>")
-   (= (first (first listEl)) 103) (str "<span style=\"color: orange\">" (apply str (second listEl)) "</span>")
+   (= (first (first listEl)) 103) (str "<span style=\"color: #e87e17\">" (apply str (second listEl)) "</span>")
    (= (first (first listEl)) 104) (str "<span style=\"color: blue\">" (apply str (second listEl)) "</span>")
    (= (first (first listEl)) 105) (str "<span style=\"color: blue\">" (apply str (second listEl)) "</span>")
    (= (first (first listEl)) 106) (if (reserved? (second listEl))
@@ -113,7 +123,7 @@
   (loop [fname filename f file s state w word r res] (when (not (nil? file))
     (cond
       (nil? f) (pwriteHTML fname (concat r (list (list (list s) w)) (list (list (list "end")))))
-      (= s -1) (recur fname (next f) 0 (concat w (list (first f))) r)
+      (= s -1) (recur fname f 0 '() (concat r (list (list (list s) w)))) ;(recur fname (next f) 0 (concat w (list (first f))) r)
       (> s 100) (if (some (fn [x] (= s x)) estados*)
         (recur fname (concat (list (first (reverse w))) f) 0 '() (concat r (list (list (list s) (reverse (next (reverse w)))))))
         (recur fname f 0 '() (concat r (list (list (list s) w)))))
